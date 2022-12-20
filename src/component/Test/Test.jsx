de5df944c91f737b9a038e35fcb3606336580f3d
ZRAIER Hamdi <hamdi.zraier@ETUDE.LOCAL>
2022-12-20 18:15:26 +0100

    het

--- src/component/Test/Test.jsx
+++ src/component/Test/Test.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react'
 
 const Test = () => {
-    const [text,setText]=useState("")
+    const [textChange,setTextChange]=useState("")
     const [counter,setCounter]=useState(0)
     const [disabled,setDisabled]=useState(false)
     useEffect(()=>{
-        if (text!==""){
+        if (textChange!==""){
      setTimeout(()=>{setCounter(counter+1)},1000)
      }
-     if (text==="il sagit dutomatiser la gestion des commandes de la societe")  {
+     if (textChange=="il sagit dutomatiser la gestion des commandes de la societe")  {
+       console.log(true)
         setDisabled(true)
     }
-
-    },[text])
+    console.log(textChange)
+    },[textChange])
 
   return (
     <div className='test'>
     <h1>il sagit dutomatiser la gestion des commandes de la societe </h1>
     <p>{counter}</p>
-    <textarea name="" id="" cols="40" rows="30"   onChange={(text)=>setText(text)} disabled={disabled}></textarea>
+    <textarea name="" id="" cols="40" rows="30"   onChange={(text)=>setTextChange(text.target.value)} disabled={disabled}></textarea>
     </div>
   )
 }
