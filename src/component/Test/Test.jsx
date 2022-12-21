4f85f83895c0f167988d40fc2aa6a9f7c94d4bde
hamdink <zraier.hamdi@gmail.com>
2022-12-21 15:21:57 +0000

    update

--- src/component/Test/Test.jsx
+++ src/component/Test/Test.jsx
@@ -4,22 +4,43 @@ const Test = () => {
     const [textChange,setTextChange]=useState("")
     const [counter,setCounter]=useState(0)
     const [disabled,setDisabled]=useState(false)
+    const [test,setTest]=useState(false)
+    const textToWrite='il sagit dutomatiser la gestion des commandes de la societe'
     useEffect(()=>{
         if (textChange!==""){
-     setTimeout(()=>{setCounter(counter+1)},1000)
+     setTimeout(()=>{setCounter((prev)=>prev+1)},1000)
      }
-     if (textChange=="il sagit dutomatiser la gestion des commandes de la societe")  {
-       console.log(true)
+     for(let i = 0;i<textChange.length;i++){
+      if(textChange.charAt(i) === textToWrite.charAt(i)){
+        setDisabled(false)
+      }else {
         setDisabled(true)
+      }
     }
-    console.log(textChange)
-    },[textChange])
+
+    },[textChange,counter])
+    const textChangeHandler = (e)=> {
+      console.log(disabled)
+      if(!disabled){
+        document.removeEventListener("keydown",function(e){
+          console.log('tt')
+        })
+        setTextChange(e.target.value)
+      }else if (disabled){
+        document.addEventListener("keydown", function(e) {
+          if (e.keyCode === 8) {
+            setTextChange((prev)=> (prev.slice(0,-1)))
+          }
+      });
+       
+      }
+      }
 
   return (
     <div className='test'>
     <h1>il sagit dutomatiser la gestion des commandes de la societe </h1>
     <p>{counter}</p>
-    <textarea name="" id="" cols="40" rows="30"   onChange={(text)=>setTextChange(text.target.value)} disabled={disabled}></textarea>
+    <textarea name="" id="" cols="40" rows="30" value={textChange}   onChange={(e)=>{textChangeHandler(e)}} ></textarea>
     </div>
   )
 }
