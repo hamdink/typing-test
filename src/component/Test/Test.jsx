88edff51d130ee048b08fe533845aef020845680
amirboukersvea <amir.bouker@sveasolar.com>
2022-12-22 14:35:54 +0100

    fixs

--- src/component/Test/Test.jsx
+++ src/component/Test/Test.jsx
@@ -1,48 +1,63 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Test = () => {
-    const [textChange,setTextChange]=useState("")
-    const [counter,setCounter]=useState(0)
-    const [disabled,setDisabled]=useState(false)
-    const [test,setTest]=useState(false)
-    const textToWrite='il sagit dutomatiser la gestion des commandes de la societe'
-    useEffect(()=>{
-        if (textChange!==""){
-     setTimeout(()=>{setCounter((prev)=>prev+1)},1000)
-     }
-     for(let i = 0;i<textChange.length;i++){
-      if(textChange.charAt(i) === textToWrite.charAt(i)){
-        setDisabled(false)
-      }else {
-        setDisabled(true)
+  const [textChange, setTextChange] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  let textToWrite =
+    "il sagit dutomatiser la gestion des commandes de la societe";
+
+  useEffect(() => {
+    if (textChange !== "") {
+      setTimeout(() => {
+        setCounter((prev) => prev + 1);
+      }, 1000);
+    }
+  }, [counter, textChange !== ""]);
+
+  useEffect(() => {
+    for (let i = 0; i < textChange.length; i++) {
+      if (textChange.charAt(i) === textToWrite.charAt(i)) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
       }
     }
+  }, [textChange]);
+  useEffect(() => {
+    if (textChange.length === textToWrite.length) {
+      // eslint-disable-next-line no-unused-expressions
+      alert(
+        "GG your score is " +
+          textChange.length / counter +
+          " caractere / second"
+      )
+        ? ""
+        : window.location.reload();
+    }
+  }, [textChange]);
 
-    },[textChange,counter])
-    const textChangeHandler = (e)=> {
-      console.log(disabled)
-      if(!disabled){
-        document.removeEventListener("keydown",function(e){
-          console.log('tt')
-        })
-        setTextChange(e.target.value)
-      }else if (disabled){
-        document.addEventListener("keydown", function(e) {
-          if (e.keyCode === 8) {
-            setTextChange((prev)=> (prev.slice(0,-1)))
-          }
-      });
-       
-      }
-      }
+  const handleText = (e) => {
+    e.persist();
+    setTextChange(e.target.value);
+  };
 
   return (
-    <div className='test'>
-    <h1>il sagit dutomatiser la gestion des commandes de la societe </h1>
-    <p>{counter}</p>
-    <textarea name="" id="" cols="40" rows="30" value={textChange}   onChange={(e)=>{textChangeHandler(e)}} ></textarea>
+    <div className="test">
+      <h1>{textToWrite}</h1>
+      <p>{counter}</p>
+      <textarea
+        maxLength={disabled ? textChange.length : 1000}
+        cols="40"
+        rows="30"
+        value={textChange}
+        onChange={(e) => {
+          handleText(e);
+        }}
+      ></textarea>
     </div>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
